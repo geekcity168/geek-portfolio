89b9b9e4c8c1f3190509c6c451e13de0c3535799
geekcity168 <sajatech001@gmail.com>
2025-07-12 21:53:10 +0300

    update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'crispy_forms',
+    'crispy_tailwind',
     'core',
     'projects',
     'blog',
